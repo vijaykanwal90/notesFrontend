ddf64555dd13849027c37bd65583833b8bb42c38
vijaykanwal <vijaykanwal90@gmail.com>
2025-07-05 18:05:22 +0530

    backend url

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,2 +1,2 @@
-const env = 'dev'; 
-export const BASE_URL = env === 'dev' ? "http://localhost:3000/api" : "https://multistepbackend.onrender.com/api";
+const env = 'production'; 
+export const BASE_URL = env === 'dev' ? "http://localhost:3000/api" : "https://notes-backend-3yyp.onrender.com/api";
